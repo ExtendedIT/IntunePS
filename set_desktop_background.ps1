c2542d51c6ac9b9e24676cf7c9c8efe939e7700a
ExtendedIT <37689878+ExtendedIT@users.noreply.github.com>
2018-03-23 15:23:24 +0700

    Add files via upload

---
+++ set_desktop_background.ps1
@@ -0,0 +1,14 @@
+﻿$username = $env:UserName
+Invoke-WebRequest -Uri "https://woodhavenpartners.sharepoint.com/:i:/g/EUHYuBPm7PtDoV2X_Lo4gYkB6ZW215InSiaOPIG8MI6HvA?e=VfXyEB" -OutFile "C:\users\$username\wallpaper.jpg"
+
+Function Set-WallPaper($Value)
+
+{
+
+ Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $value
+
+ rundll32.exe user32.dll, UpdatePerUserSystemParameters
+
+}
+
+Set-WallPaper -Value "C:\users\$username\wallpaper.jpg"
